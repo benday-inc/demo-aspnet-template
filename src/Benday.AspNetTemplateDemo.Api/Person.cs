45ceabc1e4338c4b8a13e2601fe76e5e02aafddf
Benjamin Day <benday@benday.com>
2025-04-16 08:34:19 -0400

    Added Display attributes for firstname and lastname

--- src/Benday.AspNetTemplateDemo.Api/Person.cs
+++ src/Benday.AspNetTemplateDemo.Api/Person.cs
@@ -1,11 +1,15 @@
-﻿namespace Benday.AspNetTemplateDemo.Api;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Benday.AspNetTemplateDemo.Api;
 
 public class Person
 {
     public string Id { get; set; } = Guid.NewGuid().ToString();
 
+    [Display(Name = "First Name")]
     public string FirstName { get; set; } = string.Empty;
-    
+
+    [Display(Name = "Last Name")]
     public string LastName { get; set; } = string.Empty;
     
     public List<Address> Addresses { get; set; } = new();
